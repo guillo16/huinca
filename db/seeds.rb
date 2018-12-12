a5d6e74b84ce5d2965e5086534cbeb9e0ef9ece9
guillermo varela <guillovarela16@gmail.com>
2018-12-12 17:19:03 -0300

    create routes and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 puts 'clearing database..'
-tool.destroy.all
-category.destroy.all
+Tool.destroy_all
+Category.destroy_all
 puts 'creating category..'
 stanley = Category.create!(name: 'stanley')
 dewalt = Category.create!(name: 'delwalt')
-bosch = Categoy.create!(name: 'bosch')
+bosch = Category.create!(name: 'bosch')
 puts 'creating tools...'
-Tool.create!(sku: 'martillo-stanley', name:'martillo', category: 'stanley', photo_url: 'https://http2.mlstatic.com/martillo-de-fibra-de-vidrio-stanley-51-071-profesional-D_NQ_NP_753432-MLA28987407532_122018-F.webp')
-Tool.create!(sku: 'taldro-bosch', name:'taladro', category: 'bosch', photo_url: 'https://www.bosch-professional.com/binary/ocsmedia/optimized/767x767/o4539v186_DScan.jpg')
-Tool.create!(sku: 'atornillador-dewalt', name:'atornillador', category: 'dewalt', photo_url: 'https://adamimages.sbdinc.com/GEM/Dewalt/1000x1000_72r/DCD980L2_1L.jpg')
+Tool.create!(sku: 'martillo-stanley', name:'martillo', category: stanley, photo_url: 'https://http2.mlstatic.com/martillo-de-fibra-de-vidrio-stanley-51-071-profesional-D_NQ_NP_753432-MLA28987407532_122018-F.webp')
+Tool.create!(sku: 'taldro-bosch', name:'taladro', category: bosch, photo_url: 'https://www.bosch-professional.com/binary/ocsmedia/optimized/767x767/o4539v186_DScan.jpg')
+Tool.create!(sku: 'atornillador-dewalt', name:'atornillador', category: dewalt, photo_url: 'https://adamimages.sbdinc.com/GEM/Dewalt/1000x1000_72r/DCD980L2_1L.jpg')
 puts 'finished.'
 
 
