33866c021b77949f29780e265f0b4966538910f6
guillermo varela <guillovarela16@gmail.com>
2018-12-13 21:30:39 -0300

    filters apply

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -2,7 +2,15 @@ class ToolsController < ApplicationController
   skip_before_action :authenticate_user!
   def index
     @tools = Tool.all
+    if params["name"]
+      @tools = Tool.where(name: params["name"])
+    elsif params["min_price_cents"]
+      @tools = Tool.where("price_cents BETWEEN ? AND ?", params["min_price_cents"], params["max_price_cents"])
+
+    else
+      @tools
   end
+end
 
   def show
     @tool = Tool.find(params[:id])
