7c61747216d9d7cf559f4c73046ad912d832b489
guillermo varela <guillovarela16@gmail.com>
2018-12-15 13:40:13 -0300

    finish show page

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :tool
   validates :description, length: { minimum: 20 }
-    validates :description, presence: true
-
+  validates :description, presence: true
   validates :rating, presence: true
 
 end
