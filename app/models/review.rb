1b4a6510a68775ce0050c369136255841015dea2
guillermo varela <guillovarela16@gmail.com>
2018-12-15 12:48:24 -0300

    add star to show

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :tool
   validates :description, length: { minimum: 20 }
+    validates :description, presence: true
+
+  validates :rating, presence: true
+
 end
