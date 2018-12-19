171fc7bd30662b309f03cd9fa9234d64efe6b3e1
guillermo varela <guillovarela16@gmail.com>
2018-12-19 10:26:55 -0300

    finish reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_021351) do
+ActiveRecord::Schema.define(version: 2018_12_19_124059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,9 @@ ActiveRecord::Schema.define(version: 2018_12_18_021351) do
     t.bigint "tool_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["tool_id"], name: "index_reviews_on_tool_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "tools", force: :cascade do |t|
@@ -70,5 +72,6 @@ ActiveRecord::Schema.define(version: 2018_12_18_021351) do
 
   add_foreign_key "orders", "users"
   add_foreign_key "reviews", "tools"
+  add_foreign_key "reviews", "users"
   add_foreign_key "tools", "categories"
 end
