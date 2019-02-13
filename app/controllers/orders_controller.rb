35cd6c753643751ea408c705c4092008bb0d2d5f
guillermo varela <guillovarela16@gmail.com>
2019-02-13 15:12:35 -0300

    add migration

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,8 @@
 class OrdersController < ApplicationController
+
   def create
-    tool = Tool.find(params[:tool_id])
-    order = Order.create!(tool_sku: tool.sku, amount: tool.price, state: 'pending', user: current_user)
+    tool = Tool.find(params["order"]['tool'].to_i)
+    order = Order.create!(tool_sku: tool.sku, amount: tool.price, state: 'pending', user: current_user, tool_id: tool.id)
     redirect_to new_order_payment_path(order)
   end
 
